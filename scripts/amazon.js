7f96ffd73be9d444518541b0d7ed096599dd8893
Anita David <anitafunmilayo@gmail.com>
2025-03-03 04:31:59 +0100

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -79,6 +79,11 @@ button.addEventListener('click', () => {
 		
 	}
 
-	console.log(cart);
+	let cartQuantity = 0;
+	cart.forEach((item) => {
+		cartQuantity += item.quantity;
+	});
+
+	document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 });
 });
