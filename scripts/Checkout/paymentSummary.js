c65488a6ba1ff70741f9dcd252cd2bba09c433db
Anita David <anitafunmilayo@gmail.com>
2025-03-30 04:29:58 +0100

    Create order using the backend

--- scripts/Checkout/paymentSummary.js
+++ scripts/Checkout/paymentSummary.js
@@ -2,7 +2,7 @@ import { cart, calculateCartQuantity } from "../../data/cart.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { getProduct } from "../../data/products.js";
 import { formatCurrency } from "../Utils/money.js";
-
+import { addOrder } from "../../data/orders.js";
 
 export function renderPaymentSummary() {
   let productPriceCents = 0;
@@ -59,10 +59,32 @@ export function renderPaymentSummary() {
           </div>
 
           <button class="place-order-button button- 
-					 primary">
+					 primary js-place-order">
             Place your order
           </button>
 		 `;
   document.querySelector(".js-payment-summary").innerHTML = paymentSummaryHTML;
-}
 
+  document
+    .querySelector(".js-place-order")
+    .addEventListener("click", async () => {
+      try {
+        const response = await fetch("https://supersimplebackend.dev/orders", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            cart: cart,
+          }),
+        });
+
+        const order = await response.json();
+        addOrder(order);
+      } catch (error) {
+        console.error("Unexpected error. Try again later.");
+      }
+
+      window.location.href = "orders.html";
+    });
+}
